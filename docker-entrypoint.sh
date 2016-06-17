ff38f53deba81810f42595fd5929a202849618a8
ROMAN SHESTAKOV <romanshestakov@ROMANs-iMac-2.home>
2016-06-17 22:24:09 +0100

    udpate for comment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -64,13 +64,13 @@ until curl -s -o /dev/null "http://${GO_SERVER}:${GO_SERVER_PORT}";
     echo "Waiting for http://${GO_SERVER}:${GO_SERVER_PORT}";
 done;
 
-# start agent as go user
-sudo -u ${USER_NAME} AGENT_MEM=$AGENT_MEM AGENT_MAX_MEM=$AGENT_MAX_MEM /usr/share/go-agent/agent.sh &
+# # start agent as go user
+# sudo -u ${USER_NAME} AGENT_MEM=$AGENT_MEM AGENT_MAX_MEM=$AGENT_MAX_MEM /usr/share/go-agent/agent.sh &
 
-# wait for agent to start logging
-while [ ! -f /var/log/go-agent/go-agent-bootstrapper.log ];
-    do sleep 1;
-done;
+# # wait for agent to start logging
+# while [ ! -f /var/log/go-agent/go-agent-bootstrapper.log ];
+#     do sleep 1;
+# done;
 
-# tail logs, to be replaced with logs that automatically go to stdout/stderr so go.cd crashing will crash the container
-sudo -u ${USER_NAME} exec tail -F /var/log/go-agent/*
+# # tail logs, to be replaced with logs that automatically go to stdout/stderr so go.cd crashing will crash the container
+# sudo -u ${USER_NAME} exec tail -F /var/log/go-agent/*
